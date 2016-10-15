2621b820d1c1cc17edc8c2e3f6641d551e3a954c
David Mendez Guardado <demg@outlook.com>
2016-10-15 16:48:25 -0500

    -Aplicacion Terminada. -NO SE RECOMIENDA UTILIZAR EL PLUGIN DE MYSQL QUE SE UTILIZO EN ESTA APP POR CUESTIONES DE SEGURIDAD.

--- EmergencyAmbulance_AndroidCSharp/MainActivity.cs
+++ EmergencyAmbulance_AndroidCSharp/MainActivity.cs
@@ -5,12 +5,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace EmergencyAmbulance_AndroidCSharp
 {
     [Activity(Label = "Reporte - EmergencyAmbulance", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        string s1, s2, s3;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -25,7 +28,7 @@ namespace EmergencyAmbulance_AndroidCSharp
             Spinner spinner3 = FindViewById<Spinner>(Resource.Id.spinner3);
 
             //Llenado Spinner Ambulancias
-            spinner1.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            spinner1.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner1_ItemSelected);
             var adapter1 = ArrayAdapter.CreateFromResource(
                     this, Resource.Array.ambulance_array, Android.Resource.Layout.SimpleSpinnerItem);
 
@@ -33,7 +36,7 @@ namespace EmergencyAmbulance_AndroidCSharp
             spinner1.Adapter = adapter1;
 
             //Llenado Spinner Sexo
-            spinner2.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            spinner2.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner2_ItemSelected);
             var adapter2 = ArrayAdapter.CreateFromResource(
                     this, Resource.Array.sexo, Android.Resource.Layout.SimpleSpinnerItem);
 
@@ -41,18 +44,120 @@ namespace EmergencyAmbulance_AndroidCSharp
             spinner2.Adapter = adapter2;
 
             //Llenado Spinner Tipo Sangre
-            spinner3.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            spinner3.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner3_ItemSelected);
             var adapter3 = ArrayAdapter.CreateFromResource(
                     this, Resource.Array.tipo_sangre, Android.Resource.Layout.SimpleSpinnerItem);
 
             adapter3.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             spinner3.Adapter = adapter3;
+
+            //Obtengo objetos de componentes
+            Button enviar = FindViewById<Button>(Resource.Id.btnGuardar);
+
+            enviar.Click += (object sender, EventArgs e) =>
+            {
+                string connsqlstring = "Server=192.168.0.15;Port=3306;database=emergency;User Id=root;Password=root;charset=utf8";
+                Object[] data = new Object[10];
+
+                EditText nombre = FindViewById<EditText>(Resource.Id.txtNombre);
+                EditText apellido = FindViewById<EditText>(Resource.Id.txtApellido);
+                EditText edad = FindViewById<EditText>(Resource.Id.txtEdad);
+                EditText presion = FindViewById<EditText>(Resource.Id.txtPresion);
+                EditText pulso = FindViewById<EditText>(Resource.Id.txtPulso);
+                EditText diagnostico = FindViewById<EditText>(Resource.Id.txtDiagnostico);
+
+                //Select id
+                try
+                {
+                    MySqlConnection sqlconn = new MySqlConnection(connsqlstring);
+                    sqlconn.Open();
+
+                    string Query = "SELECT idAmbulancia FROM ambulanciasdisponibles WHERE nombreAmbulancia='" + s1 + "';";
+
+                    DataTable t = new DataTable();
+                    try
+                    {
+                        MySqlDataAdapter da = new MySqlDataAdapter(Query, sqlconn);
+                        da.Fill(t);
+                        DataRow row = t.Rows[0];
+                        data[0] = row["idAmbulancia"].ToString();
+
+                    }
+                    catch (MySqlException ex)
+                    {
+
+                    }
+
+                    sqlconn.Close();
+
+                    string toast = string.Format(" Aviso: Reporte Enviado");
+                    Toast.MakeText(this, toast, ToastLength.Long).Show();
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+
+                DateTime thisDay = DateTime.Now;
+                data[1] = thisDay.Day.ToString() + "/" + thisDay.Month.ToString() + "/" + thisDay.Year.ToString() + " " + thisDay.Hour.ToString() + ":" + thisDay.Minute.ToString() + ":" + thisDay.Second.ToString();
+                data[2] = nombre.Text.ToString();
+                data[3] = apellido.Text.ToString();
+                data[4] = s2;
+                data[5] = edad.Text.ToString();
+                data[6] = presion.Text.ToString();
+                data[7] = pulso.Text.ToString();
+                data[8] = s3;
+                data[9] = diagnostico.Text.ToString();
+
+                //Insercion
+                try
+                {
+                    MySqlConnection sqlconn = new MySqlConnection(connsqlstring);
+                    sqlconn.Open();
+
+                    string nonQuery = "INSERT INTO reporte_medico(idAmbulancia,fechaReporte,nombrePaciente,apellidoPaciente,sexoPaciente,edadPaciente,presionPaciente,pulsoPaciente,sangrePaciente,diagnosticoPaciente)" +
+                " VALUES({0},'{1}','{2}','{3}','{4}',{5},'{6}','{7}','{8}','{9}');";
+                    nonQuery = String.Format(nonQuery, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]);
+
+                    MySqlCommand instruccion = new MySqlCommand(nonQuery, sqlconn);
+
+                    instruccion.ExecuteNonQuery();
+
+                    sqlconn.Close();
+
+                    string toast = string.Format(" Aviso: Reporte Enviado");
+                    Toast.MakeText(this, toast, ToastLength.Long).Show();
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                }
+            };
         }
 
-        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        private void spinner1_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
             Spinner spinner = (Spinner)sender;
 
+            s1 = "" + spinner.GetItemAtPosition(e.Position);
+            //string toast = string.Format("The planet is {0}", spinner.GetItemAtPosition(e.Position));
+            //Toast.MakeText(this, toast, ToastLength.Long).Show();
+        }
+
+        private void spinner2_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+
+            s2 = "" + spinner.GetItemAtPosition(e.Position);
+            //string toast = string.Format("The planet is {0}", spinner.GetItemAtPosition(e.Position));
+            //Toast.MakeText(this, toast, ToastLength.Long).Show();
+        }
+
+        private void spinner3_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+
+            s3 = "" + spinner.GetItemAtPosition(e.Position);
             //string toast = string.Format("The planet is {0}", spinner.GetItemAtPosition(e.Position));
             //Toast.MakeText(this, toast, ToastLength.Long).Show();
         }
